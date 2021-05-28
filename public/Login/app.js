80bb21b88a30a43110313d5e489068a16432ff8f
lohithsrk <srklohith05@gmail.com>
2021-05-28 18:18:54 +0530

    changes

--- public/Login/app.js
+++ public/Login/app.js
@@ -22,3 +22,19 @@ menu.addEventListener("click", () => {
 nav_container.addEventListener("click", () => {
     iffalse();
 });
+
+const eyes = document.querySelector(".eyesContainer");
+const eye = document.querySelector("#eye");
+const slashedEye = document.querySelector("#slashedEye");
+eyes.addEventListener("click", () => {
+    var input = document.querySelectorAll(".inputPassword");
+    input.forEach((e) => {
+        if(e.type === "password") {
+            e.type = "text";
+            slashedEye.style.opacity = "0";
+        } else {
+            e.type = "password";
+            slashedEye.style.opacity = "1";
+        }
+    });
+});
